0854da4dcc791523e1c5231c76f3fb3d446d66df
nick-leslie <nick.leslie303@gmail.com>
2024-01-20 15:11:15 -0500

    init

---
+++ apps/frontend/vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import eslint from "vite-plugin-eslint";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  resolve: {
+    preserveSymlinks: true,
+  },
+  server: {
+    host: "0.0.0.0",
+    port: parseInt(<string>process.env.PORT),
+    proxy: {
+      "/api": process.env.BACKEND_SOURCE + ":" + process.env.BACKEND_PORT,
+    },
+    watch: {
+      usePolling: true,
+    },
+  },
+  build: {
+    outDir: "build",
+  },
+  plugins: [react(), eslint()],
+});
