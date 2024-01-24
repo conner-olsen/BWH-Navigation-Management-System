8ffd7f6549b797cd19dda78c2699936e4d2ddf51
tanyaalikhan <takhan@wpi.edu>
2024-01-24 13:02:09 -0500

    added some navbar comments

--- apps/frontend/src/components/NavBar.tsx
+++ apps/frontend/src/components/NavBar.tsx
@@ -1,16 +1,20 @@
 import React from "react";
-import { Navbar, Nav } from "react-bootstrap";
-import { Link } from "react-router-dom";
-import { Outlet } from "react-router-dom";
-import { Container } from "react-bootstrap";
-
+import { Navbar, Nav } from "react-bootstrap"; //These components are used to build a responsive navigation bar.
+import { Link } from "react-router-dom"; //This is used to create links between different pages in a React application.
+import { Outlet } from "react-router-dom"; //The Outlet is a placeholder where child routes will be rendered.
+import { Container } from "react-bootstrap"; //This is used to wrap the contents of the navigation bar in a container.
+/*The Container component in Bootstrap is designed to constrain and center the content within a fixed-width container,
+making it responsive*/
 const NavBar: React.FC = () => {
   return (
     <Container fluid>
+        {/*
+        Begins a Bootstrap container that spans the entire width of the viewport.
+        */}
       <Outlet />
       <Navbar variant="light" expand="lg">
         <Nav
-          className="d-flex justify-content-between"
+          className="d-flex justify-content-center"
           style={{ width: "100%" }}
         >
           <Nav.Link className={"navUserSelect"} as={Link} to="/UserSelection">
