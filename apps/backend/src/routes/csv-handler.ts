b01694c32e3dc497da0e5b1b2600fe525cc751bb
maxgossselin <mgosselin2022@gmail.com>
2024-01-24 21:08:59 -0500

    csv-handler updated to populate edges table and nodes table. Also updated schema to have no varchar.

--- apps/backend/src/routes/csv-handler.ts
+++ apps/backend/src/routes/csv-handler.ts
@@ -1,28 +1,94 @@
 import express, {Router, Request, Response} from "express";
-import {convertToJSON, parseCSV} from "../bin/parser.ts";
+import { parseCSV} from "../bin/parser.ts";
 import * as fs from "fs";
 import * as path from "path";
+import PrismaClient from "../bin/database-connection.ts";
+
 
 
 
 const router: Router = express.Router();
 
 
+interface edge {
+  edgeid:string,
+  startnode:string,
+  endnode:string
+}
+interface node {
+  nodeId:string,
+  xcoord:number,
+  ycoord:number,
+  floor:string,
+  building:string,
+  nodeType:string,
+  longName:string,
+  shortName:string
+
+}
 
 router.post("/", async (req: Request, res: Response) => {
   try {
+    // Read the CSV string from the request body
+    const absolutePathNode = path.join(__dirname, "../../CSV-Data/L1Nodes.csv");
+    const csvFileNode = fs.readFileSync(absolutePathNode, "utf-8");
+
+    // Parse the CSV string to an array of CSVRow objects
+    const rowsNode = parseCSV(csvFileNode);
+    const transformedNode:node[] = rowsNode.map((row) => {
+      const rowval = Object.values(row);
+      return {
+        nodeId:rowval[0],
+        xcoord:parseInt(rowval[1]),
+        ycoord:parseInt(rowval[2]),
+        floor:rowval[3],
+        building:rowval[4],
+        nodeType:rowval[5],
+        longName:rowval[6],
+        shortName:rowval[7]
+      };
+    });
+
+    transformedNode.pop();
+    await PrismaClient.node.createMany({data:transformedNode.map((self) => {
+        return {
+         nodeId:self.nodeId,
+          xcoord:self.xcoord,
+          ycoord:self.ycoord,
+          floor:self.floor,
+          building:self.building,
+          nodeType:self.nodeType,
+          longName:self.longName,
+          shortName:self.shortName
+        };}
+      )
+    });
+
     // Read the CSV string from the request body
     const absolutePath = path.join(__dirname, "../../CSV-Data/L1Edges.csv");
     const csvFile = fs.readFileSync(absolutePath, "utf-8");
 
     // Parse the CSV string to an array of CSVRow objects
     const rows = parseCSV(csvFile);
-    const jsonData = convertToJSON(rows);
-    console.log(jsonData);
+    const transformed:edge[] = rows.map((row) => {
+      const rowval = Object.values(row);
+      return {
+        edgeid:rowval[0],
+        startnode:rowval[1],
+        endnode:rowval[2]
+      };
+    });
 
-    res.json(rows);
 
-
+    transformed.pop();
+    await PrismaClient.edge.createMany({data:transformed.map((self) => {
+      return {
+        startNode:self.startnode,
+        edgeID:self.edgeid,
+        endNode:self.endnode
+      };}
+      )
+    });
 
 
   } catch (error) {
