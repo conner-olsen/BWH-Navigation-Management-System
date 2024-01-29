60b590e436f5145f92056e1219b41a7e3e56d419
Karish Gupta <karishgupta2003@gmail.com>
2024-01-29 14:46:42 -0500

    minor fix for login feature

--- apps/frontend/src/routes/PatientLogin.tsx
+++ apps/frontend/src/routes/PatientLogin.tsx
@@ -31,7 +31,7 @@ const PatientLogin = () => {
             navigate("/");
         }
         else {
-            navigate("/AdminLogin");
+            navigate("/PatientLogin");
         }
 
     };
