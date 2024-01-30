a6d483d1e0cb6a0a73edd8dbb3c7dae7d935aa05
tanyaalikhan <takhan@wpi.edu>
2024-01-30 18:59:24 -0500

    removed sign in image, added all services to drop down, updated styling

--- apps/frontend/src/components/NavBar.tsx
+++ apps/frontend/src/components/NavBar.tsx
@@ -10,28 +10,16 @@ export default function NavBar() {
                 className="generalNavBar"
             >
                 <Nav.Link className={"navMap"} as={Link} to="/">
-                    <button className="btn btn-secondary" type="button" id="dropdownMenuButton2"
-                            data-bs-toggle="dropdown" aria-expanded="false">
                         Home
-                    </button>
                 </Nav.Link>
                 <Nav.Link className={"navMap"} as={Link} to="/UserSelection">
-                    <button className="btn btn-secondary" type="button" id="dropdownMenuButton2"
-                            data-bs-toggle="dropdown" aria-expanded="false">
                         Login
-                    </button>
                 </Nav.Link>
                 <Nav.Link className={"navMap"} as={Link} to="/map">
-                    <button className="btn btn-secondary" type="button" id="dropdownMenuButton2"
-                            data-bs-toggle="dropdown" aria-expanded="false">
                         Map
-                    </button>
                 </Nav.Link>
                 <Nav.Link className={"navMap"} as={Link} to="/bfs">
-                    <button className="btn btn-secondary" type="button" id="dropdownMenuButton2"
-                            data-bs-toggle="dropdown" aria-expanded="false">
                         Breadth-First Search
-                    </button>
                 </Nav.Link>
                 <NavDropdown title="Service Requests" id="basic-nav-dropdown">
                     <NavDropdown.Item as={Link} to="/ServiceList">
@@ -53,6 +41,9 @@ export default function NavBar() {
                     </NavDropdown.Item>
                     <NavDropdown.Item as={Link} to="/UserSelection">
                         Login
+                    </NavDropdown.Item>
+                    <NavDropdown.Item as={Link} to="/UserSelection">
+                        All Services
                     </NavDropdown.Item>
                     <NavDropdown.Item as={Link} to="/map">
                         Map
