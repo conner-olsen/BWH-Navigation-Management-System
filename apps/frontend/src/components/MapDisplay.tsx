c62a2f478044da760eb8f698ba204a48cd580c8c
wuzicant <steven.gao@lca.edu>
2024-01-31 15:48:19 -0500

    shows both nodes and edges on the map

--- apps/frontend/src/components/MapDisplay.tsx
+++ apps/frontend/src/components/MapDisplay.tsx
@@ -8,10 +8,28 @@ function MapDisplay() {
     useEffect(() => {
         setGraph(populatedGraph);
     }, []);
+    const displayEdges = (graph: Graph) => {
+        let edges = [];
+        for (let [nodeId, node] of graph.nodes) {
+            node.edges.forEach(edgeNodeId => {
+                let targetNode = graph.getNode(edgeNodeId);
+                if (targetNode) {
+                    edges.push(
+                        <line key={`${nodeId}-${edgeNodeId}`}
+                              x1={node.xCoord} y1={node.yCoord}
+                              x2={targetNode.xCoord} y2={targetNode.yCoord}
+                              stroke="black" strokeWidth="1"/>
+                    );
+                }
+            });
+        }
+        return edges;
+    };
 
     return (
         <svg x="0" y="0" viewBox="0 0 5000 3400">
             <image href="../../public/maps/L1map.png" width="5000" height="3400" x="0" y="0"/>
+            {graph && displayEdges(graph)}
             {graph && Array.from(graph.nodes.values()).map((node: Node) => (
                 <circle key={node.id} cx={node.xCoord} cy={node.yCoord} r="5" fill="red" />
             ))}
