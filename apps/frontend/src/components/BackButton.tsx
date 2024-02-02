baffcc10daf0190181801641cbbbc2ee30e0b571
smithwm210 <smithwm210@gmail.com>
2024-02-01 19:59:00 -0500

    updated back button

--- apps/frontend/src/components/BackButton.tsx
+++ apps/frontend/src/components/BackButton.tsx
@@ -1,18 +1,19 @@
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 interface BackButton {
     link: string;
 }
 
-
-const BackButton: React.FC<BackButton> = ({ link }) => {
-  return (
-      <Link to={link}>
-        <button className= {"BackButton"}>
-          <img className="BackButtonImg" src="public/icon/Vector.png" alt="" />
-        </button>
-      </Link>
-  );
+const BackButton: React.FC<BackButton> = () => {
+    const navigate = useNavigate();
+    return (
+        <div>
+            <button onClick={() => navigate(-1)} className= {"BackButton"}>
+                <img className="BackButtonImg" src="public/icon/Vector.png" alt="" />
+            </button>
+        </div>
+    );
 };
 
 export default BackButton;
+
