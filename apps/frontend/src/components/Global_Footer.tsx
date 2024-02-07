e6314d4414743d68189c4c18fbf587e14563f1f7
mlnbui <banhmistaff@gmail.com>
2024-02-06 19:43:57 -0500

    Navbar rework

--- apps/frontend/src/components/Global_Footer.tsx
+++ apps/frontend/src/components/Global_Footer.tsx
@@ -4,16 +4,17 @@ import { Link } from "react-router-dom";
 // This is a basic header that should be shown on every page of the site
 export default function GlobalFooter() {
   return (
-    <footer className="flex flex-col flex-grow-1 justify-end w-full">
-        <div className="container flex justify-between">
-            <p className="text-xs m-0">© 2024 Brigham and Women's Hospital</p>
-            <div>
-                <Link to="/" className="pl-1 inline-block no-underline text-xs">Link 1</Link>
-                <Link to="/" className="pl-1 inline-block no-underline text-xs">Link 2</Link>
-                <Link to="/" className="pl-1 inline-block no-underline text-xs">Link 3</Link>
-                <Link to="/" className="pl-1 inline-block no-underline text-xs">Link 4</Link>
-            </div>
-        </div>
-    </footer>
+      <footer className="flex flex-col flex-grow-1 justify-end w-full py-2">
+          <hr className="my-2 container"/>
+          <div className="container flex justify-between">
+              <p className="text-xs m-0">© 2024 Brigham and Women's Hospital</p>
+              <div className="flex items-center">
+                  <Link to="/" className="pl-1 inline-block no-underline text-xs">Link 1</Link>
+                  <Link to="/" className="pl-1 inline-block no-underline text-xs">Link 2</Link>
+                  <Link to="/" className="pl-1 inline-block no-underline text-xs">Link 3</Link>
+                  <Link to="/" className="pl-1 inline-block no-underline text-xs">Link 4</Link>
+              </div>
+          </div>
+      </footer>
   );
 };
--- apps/frontend/src/components/NavBar.tsx
+++ apps/frontend/src/components/NavBar.tsx
@@ -1,13 +1,12 @@
 import React from "react";
 import {Nav, NavDropdown} from "react-bootstrap";
+import  DarkModeButton  from "./DarkModeButton.tsx";
 import { Link } from "react-router-dom";
-import { useState } from "react";
+
 
 export default function NavBar() {
-    const [, setIsHovered] = useState(false);
 
     const handleMouseEnter = () => {
-        setIsHovered(true);
         const customElement = document.getElementById('bg-blur');
         if (customElement) {
             customElement.classList.remove('h-0');
@@ -16,7 +15,6 @@ export default function NavBar() {
     };
 
     const handleMouseLeave = () => {
-        setIsHovered(false);
         const customElement = document.getElementById('bg-blur');
         if (customElement) {
             customElement.classList.remove('h-screen');
@@ -27,27 +25,31 @@ export default function NavBar() {
 
     return (
         <nav>
-            <Nav className="navbarStyling relative filter-none z-10">
-                <Nav.Link as={Link} to="/Home">Home</Nav.Link>
-                <Nav.Link as={Link} to="/NodeData">Node Data</Nav.Link>
-                <Nav.Link as={Link} to="/EdgeData">Edge Data</Nav.Link>
-                <Nav.Link as={Link} to="/ServiceList" className="group text-center"
+            <Nav className="navbarStyling relative filter-none z-10
+                            shadow-md">
+                <Link to="/Home" className="text-sm no-underline p-2">Home</Link>
+                <Link to="/NodeData" className="text-sm no-underline p-2">Node Data</Link>
+                <Link to="/EdgeData" className="text-sm no-underline p-2">Edge Data</Link>
+                <Link to="/ServiceList" className="group text-center text-sm no-underline p-2"
                           onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
                     Service Request
-                    <div className="h-0 w-screen max-w-full group-hover:h-28 absolute
-                                    bg-white left-0 overflow-hidden
+                    <div className="mt-7 h-0 w-screen max-w-full group-hover:h-[20vh] absolute
+                                    bg-white left-0 overflow-hidden flex justify-center
                                     transition-all duration-500">
-                        <NavDropdown.Item as={Link} to="/ServiceList">All Services</NavDropdown.Item>
-                        <NavDropdown.Item as={Link} to="/FlowerService">Send Flowers</NavDropdown.Item>
-                        <NavDropdown.Item as={Link} to="/ReligiousService">Religious Services</NavDropdown.Item>
-                        <NavDropdown.Item as={Link} to="/CleaningService">Cleaning Services</NavDropdown.Item>
-                        <NavDropdown.Item as={Link} to="/ServiceLog">Service Log</NavDropdown.Item>
+                        <div>
+                            <NavDropdown.Item as={Link} to="/ServiceList">All Services</NavDropdown.Item>
+                            <NavDropdown.Item as={Link} to="/FlowerService">Send Flowers</NavDropdown.Item>
+                            <NavDropdown.Item as={Link} to="/ReligiousService">Religious Services</NavDropdown.Item>
+                            <NavDropdown.Item as={Link} to="/CleaningService">Cleaning Services</NavDropdown.Item>
+                            <NavDropdown.Item as={Link} to="/ServiceLog">Service Log</NavDropdown.Item>
+                        </div>
+
                     </div>
 
-                </Nav.Link>
-                <Nav.Link as={Link} to="/Home" className="group text-center"
+                </Link>
+                <Link to="/Home" className="group text-center text-sm no-underline p-2"
                           onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>More
-                    <div className="h-0 w-screen max-w-full group-hover:h-28 absolute
+                    <div className="mt-7 h-0 w-screen max-w-full group-hover:h-[20vh] absolute
                                     bg-white left-0 overflow-hidden
                                     transition-all duration-500">
                         <NavDropdown.Item as={Link} to="/Home" className="nav-drop-down-link">Home</NavDropdown.Item>
@@ -62,8 +64,10 @@ export default function NavBar() {
                             Page</NavDropdown.Item>
                     </div>
 
-                </Nav.Link>
+                </Link>
+                <DarkModeButton></DarkModeButton>
             </Nav>
+
             <div id="bg-blur" className="h-0 w-screen max-w-full max-h-full absolute
                                         left-0 backdrop-blur-sm
                                         transition-all duration-500"></div>
