0e648801f9ffcb06146fc3929529198de8405c4f
Karish Gupta <karishgupta2003@gmail.com>
2024-02-07 18:41:27 -0500

    Navbar changes

--- apps/frontend/src/components/NavBar.tsx
+++ apps/frontend/src/components/NavBar.tsx
@@ -38,38 +38,35 @@ export default function NavBar() {
 
     return (
         <nav>
-            <Nav className="navbarStyling relative filter-none z-10
-                            shadow-md">
-                <Link to="/Home" className="text-sm no-underline p-2">Home</Link>
-                <Link to="/MapPage" className="text-sm no-underline p-2">Map Page</Link>
+            <Nav className="navbarStyling relative filter-none z-1 shadow-md bg-dark-blue">
+                <Link to="/Home" className="text-lg no-underline p-2 text-white hover:bg-sky-700">Home</Link>
+                <Link to="/MapPage" className="text-lg no-underline p-2 text-white hover:bg-sky-700">Map Page</Link>
 
                 {isAuthenticated && (
-                <Link to="/NodeData" className="text-sm no-underline p-2">Node Data</Link>
+                    <Link to="/NodeData" className="text-lg no-underline p-2 text-white hover:bg-sky-700">Node Data</Link>
                 )}
                 {isAuthenticated && (
-                <Link to="/EdgeData" className="text-sm no-underline p-2">Edge Data</Link>
+                    <Link to="/EdgeData" className="text-lg no-underline p-2 text-white hover:bg-sky-700">Edge Data</Link>
                 )}
                 {isAuthenticated && (
-                <Link to="/ServiceList" className="group text-center text-sm no-underline p-2"
+                    <Link to="/ServiceList" className="group text-center text-lg no-underline p-2 text-white hover:bg-sky-700"
                           onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
-                    Service Request
-                    <div className="mt-7 h-0 w-screen max-w-full group-hover:h-[20vh] absolute
-                                    bg-white left-0 overflow-hidden flex justify-center
+                        Service Request
+                        <div className="mt-7 h-0 w-screen max-w-full group-hover:h-[20vh] absolute
+                                    bg-dark-blue left-0 overflow-hidden flex justify-center
                                     transition-all duration-500">
-                        <div>
-                            <NavDropdown.Item as={Link} to="/ServiceList">All Services</NavDropdown.Item>
-                            <NavDropdown.Item as={Link} to="/FlowerService">Send Flowers</NavDropdown.Item>
-                            <NavDropdown.Item as={Link} to="/ReligiousService">Religious Services</NavDropdown.Item>
-                            <NavDropdown.Item as={Link} to="/CleaningService">Cleaning Services</NavDropdown.Item>
-                            <NavDropdown.Item as={Link} to="/ServiceLog">Service Log</NavDropdown.Item>
+                            <div>
+                                <NavDropdown.Item as={Link} to="/ServiceList">All Services</NavDropdown.Item>
+                                <NavDropdown.Item as={Link} to="/FlowerService">Send Flowers</NavDropdown.Item>
+                                <NavDropdown.Item as={Link} to="/ReligiousService">Religious Services</NavDropdown.Item>
+                                <NavDropdown.Item as={Link} to="/CleaningService">Cleaning Services</NavDropdown.Item>
+                                <NavDropdown.Item as={Link} to="/ServiceLog">Service Log</NavDropdown.Item>
+                            </div>
+
                         </div>
 
-                    </div>
-
-                </Link>
+                    </Link>
                 )}
-
-                <DarkModeButton></DarkModeButton>
 
                 {isAuthenticated && (
                     <Dropdown id={"dropdown-button"} className="text-sm no-underline p-2">
@@ -83,12 +80,14 @@ export default function NavBar() {
                         </Dropdown.Menu>
 
 
-                        </Dropdown>
+                    </Dropdown>
                 )}
 
                 {!isAuthenticated && (
-                <Button onClick={() => loginWithRedirect()}>Log in</Button>
+                    <Button onClick={() => loginWithRedirect()}>Log in</Button>
                 )}
+
+                <DarkModeButton></DarkModeButton>
 
             </Nav>
 
