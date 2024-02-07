949740e2c79551eab1a6b64a9a73d153fcbdbc7c
mlnbui <banhmistaff@gmail.com>
2024-02-06 21:04:36 -0500

    Form rework

--- apps/frontend/src/routes/ServiceRequests/FlowerServiceRequest.tsx
+++ apps/frontend/src/routes/ServiceRequests/FlowerServiceRequest.tsx
@@ -1,5 +1,3 @@
-
-import BackButton from "../../components/BackButton.tsx";
 import NavBar from "../../components/NavBar.tsx";
 import React, { useState } from 'react';
 import axios from "axios";
@@ -73,90 +71,94 @@ const FlowerServiceRequest: React.FC = () => {
 
     return (
         <div>
-            <BackButton></BackButton>
             <NavBar></NavBar>
             <button className="inline-block p-2.5 text-center text-light-blue cursor-pointer
                            border-light-blue rounded-md border-solid border-2
                            transition-all transition-duration-300
-                           hover:bg-light-blue hover:text-white" onClick={populateEmployeeTable}>Populate Employee Table</button>
-            <h1 className={"pageHeader"}>Flower Delivery Form</h1>
-            <br/>
-            <form className={"flowerService"} onSubmit={handleSubmit}>
-                <div>
-                    <label htmlFor="senderName">Sender Name</label>
-                    <input
-                        type="text"
-                        id="senderName"
-                        name="senderName"
-                        placeholder={"John Doe"}
-                        required
-                        value={formData.senderName}
-                        onChange={handleChange}
-                    />
-                    <label htmlFor="senderEmail">Sender Email</label>
-                    <input
-                        type="text"
-                        id="senderEmail"
-                        name="senderEmail"
-                        placeholder={"John@gmail.com"}
-                        required
-                        value={formData.senderEmail}
-                        onChange={handleChange}
-                    />
+                           hover:bg-light-blue hover:text-white" onClick={populateEmployeeTable}>Populate Employee Table
+            </button>
+            <div className="container">
+                <h1 className="font-roboto font-bold font-6xl">FLOWER DELIVERY FORM</h1>
+                <form onSubmit={handleSubmit}>
+                    <div className="flex justify-between">
+                        <div className="flex flex-col">
+                            <label htmlFor="senderName">Sender Name</label>
+                            <input
+                                type="text"
+                                id="senderName"
+                                name="senderName"
+                                placeholder={"John Doe"}
+                                required
+                                value={formData.senderName}
+                                onChange={handleChange}
+                            />
+                            <label htmlFor="senderEmail">Sender Email</label>
+                            <input
+                                type="text"
+                                id="senderEmail"
+                                name="senderEmail"
+                                placeholder={"John@gmail.com"}
+                                required
+                                value={formData.senderEmail}
+                                onChange={handleChange}
+                            />
 
-                    <label htmlFor="roomLongName">Room Name</label>
-                    <input
-                        type="text"
-                        id="roomLongName"
-                        name="roomLongName"
-                        placeholder={"Anesthesia Conf Floor L1 (Node longName)"}
-                        required
-                        value={formData.roomLongName}
-                        onChange={handleChange}
-                    />
-                    <label htmlFor="patientName">Patient's Name</label>
-                    <input
-                        type="text"
-                        id="patientName"
-                        name="patientName"
-                        placeholder="Jared Smith"
-                        required
-                        value={formData.patientName}
-                        onChange={handleChange}
-                    />
+                            <label htmlFor="roomLongName">Room Name</label>
+                            <input
+                                type="text"
+                                id="roomLongName"
+                                name="roomLongName"
+                                placeholder={"Anesthesia Conf Floor L1 (Node longName)"}
+                                required
+                                value={formData.roomLongName}
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <div className="flex flex-col">
+                            <label htmlFor="patientName">Patient's Name</label>
+                            <input
+                                type="text"
+                                id="patientName"
+                                name="patientName"
+                                placeholder="Jared Smith"
+                                required
+                                value={formData.patientName}
+                                onChange={handleChange}
+                            />
 
-                    <label htmlFor="flowerType">Select the type of flowers</label>
-                    <select
-                        id="flowerType"
-                        name="flowerType"
-                        required
-                        className={"flowerTypeForm"}
-                        value={formData.flowerType}
-                        onChange={handleChange}
-                    >
-                        <option value="/">Select</option>
-                        <option value="daffodils">Daffodil</option>
-                        <option value="daisies">Daisies</option>
-                        <option value="hydrangeas">Hydrangeas</option>
-                        <option value="lilies">Lilies</option>
-                        <option value="marigolds">Marigolds</option>
-                        <option value="orchids">Orchid</option>
-                        <option value="roses">Roses</option>
-                        <option value="sunflowers">Sunflowers</option>
-                        <option value="tulips">Tulips</option>
-                    </select>
+                            <label htmlFor="flowerType">Select the type of flowers</label>
+                            <select
+                                id="flowerType"
+                                name="flowerType"
+                                required
+                                className={"flowerTypeForm"}
+                                value={formData.flowerType}
+                                onChange={handleChange}
+                            >
+                                <option value="/">Select</option>
+                                <option value="daffodils">Daffodil</option>
+                                <option value="daisies">Daisies</option>
+                                <option value="hydrangeas">Hydrangeas</option>
+                                <option value="lilies">Lilies</option>
+                                <option value="marigolds">Marigolds</option>
+                                <option value="orchids">Orchid</option>
+                                <option value="roses">Roses</option>
+                                <option value="sunflowers">Sunflowers</option>
+                                <option value="tulips">Tulips</option>
+                            </select>
 
-                    <label htmlFor="deliveryDateTime">Date of Delivery</label>
-                    <input
-                        type="text"
-                        id="deliveryDate"
-                        name="deliveryDate"
-                        placeholder={"01/15/1981"}
-                        required
-                        value={formData.deliveryDate}
-                        onChange={handleChange}
-                    />
-
+                            <label htmlFor="deliveryDateTime">Date of Delivery</label>
+                            <input
+                                type="text"
+                                id="deliveryDate"
+                                name="deliveryDate"
+                                placeholder={"01/15/1981"}
+                                required
+                                value={formData.deliveryDate}
+                                onChange={handleChange}
+                            />
+                        </div>
+                    </div>
                     <label>Add a note</label>
                     <textarea
                         placeholder={"I heard you're going through tough times. Get well soon!"}
@@ -168,8 +170,10 @@ const FlowerServiceRequest: React.FC = () => {
                     <br/>
                     <br/>
                     <input type="submit" value="Submit"/>
-                </div>
-            </form>
+
+                </form>
+            </div>
+
         </div>
     );
 };
