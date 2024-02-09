0fe0e8932ac12f048be493a1b983e74da5d8a41f
Karish Gupta <karishgupta2003@gmail.com>
2024-02-09 13:53:48 -0500

    Implemented cards for the service request page

--- apps/frontend/src/routes/ServiceRequests/ServiceList.tsx
+++ apps/frontend/src/routes/ServiceRequests/ServiceList.tsx
@@ -2,6 +2,10 @@
 import React from 'react';
 import NavBar from "../../components/NavBar.tsx";
 import { Link } from 'react-router-dom';
+import { Card, CardHeader, CardTitle, CardDescription, CardContent } from  "../../components/ui/card.tsx";
+import {Col, Container, Row} from "react-bootstrap";
+//import cn from "../../lib/utils.ts";
+
 
 const ServiceList = () => {
     return (
@@ -96,9 +100,103 @@ const ServiceList = () => {
                     >
                         EXTERNAL HOSPITAL TRANSPORTATION
                     </Link>
+
+
                 </div>
 
             </div>
+            <Container>
+                <Row>
+                    <Col>
+                        <Link to="/flowerService" className={"no-underline"}>
+                            <Card className="max-w-md border-gray-200">
+                                <CardHeader>
+                                    <CardTitle className="text-xl">Flower Service Request</CardTitle>
+                                    <CardDescription className="text-sm">Send a bogue of flowers to send your regards to any patient on long term stay </CardDescription>
+                                </CardHeader>
+                                <CardContent>
+                                    <img src="public/flower_service.jpg" alt="flower service"/>
+                                </CardContent>
+                            </Card>
+                        </Link>
+                    </Col>
+                    <Col>
+
+                        <Link to="/flowerService" className={"no-underline"}>
+                            <Card className="max-w-md border-gray-200">
+                                <CardHeader>
+                                    <CardTitle className="text-xl">Cleaning Service Request</CardTitle>
+                                    <CardDescription className="text-sm">Send a cleaning request for quick, deep, or emergency cleaning for patients on long term stay</CardDescription>
+                                </CardHeader>
+                                <CardContent>
+                                    <img src="public/cleaning_request.webp" alt="cleaning service"/>
+                                </CardContent>
+                            </Card>
+                        </Link>
+                    </Col>
+
+                    <Col>
+
+                        <Link to="/flowerService" className={"no-underline"}>
+                            <Card className="max-w-md border-gray-200">
+                                <CardHeader>
+                                    <CardTitle className="text-xl">Religious Service Request</CardTitle>
+                                    <CardDescription className="text-sm">Send a request for religious sermons and rituals conducted for patients on long terms stay </CardDescription>
+                                </CardHeader>
+                                <CardContent>
+                                    <img src="public/religous_service.jpg" alt="Religious service"/>
+                                </CardContent>
+                            </Card>
+                        </Link>
+                    </Col>
+                </Row>
+
+                <br/>
+
+                <Row>
+                    <Col>
+                        <Link to="/flowerService" className={"no-underline"}>
+                            <Card className="max-w-md border-gray-200">
+                                <CardHeader>
+                                    <CardTitle className="text-xl">Medication Delivery Service Request</CardTitle>
+                                    <CardDescription className="text-sm">Send a request for medication delivery for patients on long terms stay </CardDescription>
+                                </CardHeader>
+                                <CardContent>
+                                    <img src="public/medicine.jpg" alt="medicine delivery service"/>
+                                </CardContent>
+                            </Card>
+                        </Link>
+                    </Col>
+
+                    <Col>
+                        <Link to="/flowerService" className={"no-underline"}>
+                            <Card className="max-w-md border-gray-200">
+                                <CardHeader>
+                                    <CardTitle className="text-xl">Medication Delivery Service Request</CardTitle>
+                                    <CardDescription className="text-sm">Send a request for medication delivery for patients on long terms stay </CardDescription>
+                                </CardHeader>
+                                <CardContent>
+                                    <img src="public/internal.jpg" alt="medicine delivery service"/>
+                                </CardContent>
+                            </Card>
+                        </Link>
+                    </Col>
+
+                    <Col>
+                        <Link to="/flowerService" className={"no-underline"}>
+                            <Card className="max-w-md border-gray-200">
+                                <CardHeader>
+                                    <CardTitle className="text-xl">Medication Delivery Service Request</CardTitle>
+                                    <CardDescription className="text-sm">Send a request for medication delivery for patients on long terms stay </CardDescription>
+                                </CardHeader>
+                                <CardContent>
+                                    <img src="public/ambulance.jpg" alt="medicine delivery service"/>
+                                </CardContent>
+                            </Card>
+                        </Link>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 };
