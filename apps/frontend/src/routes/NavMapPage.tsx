11097ca52c50a6873233b3e8f37ab46543f58b3b
mlnbui <banhmistaff@gmail.com>
2024-02-09 14:44:10 -0500

    Updated Map Nav page

--- apps/frontend/src/routes/NavMapPage.tsx
+++ apps/frontend/src/routes/NavMapPage.tsx
@@ -16,36 +16,46 @@ const NavMapPage = () => {
             <div className="flex justify-between items-center z-[9] relative bottom-[300px]
                             hover:mt-[200px] hover:mb-[200px] transition-all duration-300">
                 <h2>Floor 2</h2>
-                <img src={`public/maps/02_thesecondfloor.png`}
-                     className="img-rotate"/>
+                <Link to={"/MapPage"} className="contents">
+                    <img src={`public/maps/02_thesecondfloor.png`}
+                         className="img-rotate"/>
+                </Link>
             </div>
 
             <div className="flex justify-between items-center z-[8] relative bottom-[600px]
                             hover:mt-[200px] hover:mb-[200px] transition-all duration-300">
                 <h2>Floor 1</h2>
-                <img src={`public/maps/01_thefirstfloor.png`}
-                     className="img-rotate"/>
+                <Link to={"/MapPage"} className="contents">
+                    <img src={`public/maps/01_thefirstfloor.png`}
+                         className="img-rotate"/>
+                </Link>
             </div>
 
-            <div className="flex justify-between items-center z-[7] relative bottom-[900px]
-                            hover:mt-[200px] hover:mb-[200px] transition-all duration-300">
-                <h2>Ground Floor</h2>
-                <img src={`public/maps/00_thegroundfloor.png`}
-                     className="img-rotate"/>
-            </div>
+            {/*<div className="flex justify-between items-center z-[7] relative bottom-[900px]*/}
+            {/*                hover:mt-[200px] hover:mb-[200px] transition-all duration-300">*/}
+            {/*    <h2>Ground Floor</h2>*/}
+            {/*    <Link to={"/MapPage"} className="contents">*/}
+            {/*        <img src={`public/maps/00_thegroundfloor.png`}*/}
+            {/*             className="img-rotate"/>*/}
+            {/*    </Link>*/}
+            {/*</div>*/}
 
-            <div className="flex justify-between items-center z-[6] relative bottom-[1200px]
+            <div className="flex justify-between items-center z-[6] relative bottom-[900px]
                             hover:mt-[200px] hover:mb-[200px] transition-all duration-300">
                 <h2>Floor L1</h2>
-                <img src={`public/maps/00_thelowerlevel1.png`}
-                     className="img-rotate"/>
+                <Link to={"/MapPage"} className="contents">
+                    <img src={`public/maps/00_thelowerlevel1.png`}
+                         className="img-rotate"/>
+                </Link>
             </div>
 
-            <div className="flex justify-between items-center z-[5] relative bottom-[1500px]
+            <div className="flex justify-between items-center z-[5] relative bottom-[1200px]
                             hover:mt-[200px] transition-all duration-300">
                 <h2>Floor L2</h2>
-                <img src={`public/maps/00_thelowerlevel2.png`}
-                     className="img-rotate"/>
+                <Link to={"/MapPage"} className="contents">
+                    <img src={`public/maps/00_thelowerlevel2.png`}
+                         className="img-rotate"/>
+                </Link>
             </div>
         </div>
     );
