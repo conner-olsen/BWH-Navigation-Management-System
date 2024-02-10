13b49fcffc1b52d1cb8bc3a3e0eaf731e62177fa
Karish Gupta <karishgupta2003@gmail.com>
2024-02-10 00:32:32 -0500

    Fixed Navbar

--- apps/frontend/src/components/ui/dropdown-menu.tsx
+++ apps/frontend/src/components/ui/dropdown-menu.tsx
@@ -3,7 +3,6 @@ import * as DropdownMenuPrimitive from "@radix-ui/react-dropdown-menu";
 import { Check, ChevronRight, Circle } from "lucide-react";
 
 import cn from "../../lib/utils.ts";
-
 const DropdownMenu = DropdownMenuPrimitive.Root;
 
 const DropdownMenuTrigger = DropdownMenuPrimitive.Trigger;
