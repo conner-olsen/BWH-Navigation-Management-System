1c7735256f22ae18cc3336ac2070c2912f164c69
aiden deady <acdeady@wpi.edu>
2024-02-12 20:06:49 -0500

    shad stuff

--- apps/frontend/src/components/ui/button.tsx
+++ apps/frontend/src/components/ui/button.tsx
@@ -37,7 +37,7 @@ export interface ButtonProps
     VariantProps<typeof buttonVariants> {
   asChild?: boolean
 }
-
+//test
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
     ({ className, asChild = false, ...props }, ref) => {
         const Comp = asChild ? Slot : "button";
