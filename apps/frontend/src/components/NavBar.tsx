5320cf46a30dbbca1a274fb1de5a7fb8c9393408
mlnbui <banhmistaff@gmail.com>
2024-02-12 22:00:53 -0500

    Navbar

--- apps/frontend/src/components/NavBar.tsx
+++ apps/frontend/src/components/NavBar.tsx
@@ -53,8 +53,6 @@ return (
         <Nav className="navbarStyling relative filter-none z-50 shadow-md bg-blue-950 space-x-12 items-center">
             <Link to="/"><img src="public/BWH_Logo_Sheild.png" className="max-w-[50px] py-1"></img></Link>
             <Link to="/Home" className="text-lg no-underline p-2 text-white"> <div className={"hover:text-blue-500"}>Home</div></Link>
-            <Link to="/MapPage" className="text-lg no-underline p-2 text-white"><div className={"hover:text-blue-500"}>Map Page</div></Link>
-
             {isAuthenticated && (
                 <Link to="/DataUpload" className="text-lg no-underline p-2 text-white"><div className={"hover:text-blue-500"}>Data Upload</div></Link>
             )}
