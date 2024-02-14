41aa93365651f5ed41dba7704905234533cf22c1
maxgossselin <mgosselin2022@gmail.com>
2024-02-13 19:10:53 -0500

    added

--- apps/frontend/src/routes/EmployeeManagement.tsx
+++ apps/frontend/src/routes/EmployeeManagement.tsx
@@ -125,6 +125,7 @@ export const EmployeeManager = () => {
                         </TabsContent>
                         <TabsContent value="Create Employee">
                             <Container>
+                                <GetDataEmployees></GetDataEmployees>
                                 <div className="max-w-md mx-auto bg-white rounded-lg px-8 pt-6 pb-8 mb-4">
                                     <form onSubmit={handleCreateEmployee}>
                                         <div className="mb-4">
