224fe36fed978ffdec7aca66fe96f65571713a3b
maxgossselin <mgosselin2022@gmail.com>
2024-02-13 20:28:47 -0500

    added

--- apps/frontend/src/routes/EmployeeManagement.tsx
+++ apps/frontend/src/routes/EmployeeManagement.tsx
@@ -1,4 +1,4 @@
-import { Container } from "react-bootstrap";
+import {Col, Container, Row } from "react-bootstrap";
 import NavBar from "../components/NavBar.tsx";
 import {Tabs, TabsContent, TabsList, TabsTrigger} from "../components/ui/tabs.tsx";
 import {GetDataEmployees} from "../components/EmployeeManagerComponent.tsx";
@@ -90,124 +90,123 @@ export const EmployeeManager = () => {
                 <Outlet/>
                 <NavBar/>
                 <div className={"align-content-center"}>
-                    <Tabs defaultValue="node" className="w-[400px]">
+                    <Tabs defaultValue="node" className="w-screen">
                         <TabsList>
-                            <TabsTrigger value="Employee List">Employee List</TabsTrigger>
+                            {/*<TabsTrigger value="Employee List">Employee List</TabsTrigger>*/}
                             <TabsTrigger value="Create Employee">Create Employee</TabsTrigger>
                             <TabsTrigger value="Update Employee">Update Employee</TabsTrigger>
+                            <TabsTrigger value="Delete Employee">Delete Employee</TabsTrigger>
                         </TabsList>
 
-                        <TabsContent value="Employee List">
-                            <Container>
-                                <GetDataEmployees></GetDataEmployees>
-                                <div className="max-w-md mx-auto bg-white rounded-lg px-8 pt-6 pb-8 mb-4">
-                                    <form onSubmit={handleDeleteEmployee}>
-                                        <div className="mb-4">
-                                            <label className="block text-gray-700 text-sm font-bold mb-2"
-                                                   htmlFor="username">
-                                                Delete User by Entering Username
-                                            </label>
-                                            <input
-                                                className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-blue-500"
-                                                id="username"
-                                                type="text"
-                                                placeholder="Username"
-                                                name="username"
-                                                value={formData.username}
-                                                onChange={(e) => setFormData({...formData, username: e.target.value})}
-                                            />
-                                        </div>
-                                        <Button variant={"ghost"}>Delete</Button>
-
-                                    </form>
-                                </div>
-                            </Container>
-                        </TabsContent>
+                        {/*<TabsContent value="Employee List">*/}
                         <TabsContent value="Create Employee">
-                            <Container>
-                                <GetDataEmployees></GetDataEmployees>
-                                <div className="max-w-md mx-auto bg-white rounded-lg px-8 pt-6 pb-8 mb-4">
-                                    <form onSubmit={handleCreateEmployee}>
-                                        <div className="mb-4">
-                                            <label className="block text-gray-700 text-sm font-bold mb-2"
-                                                   htmlFor="username">
-                                                Username
-                                            </label>
-                                            <input
-                                                className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-blue-500"
-                                                id="username"
-                                                type="text"
-                                                placeholder="Username"
-                                                name="username"
-                                                value={formData.username}
-                                                onChange={(e) => setFormData({
-                                                            ...formData,
-                                                            username: e.target.value
-                                                        })}
-                                                    />
-                                                </div>
-                                                <div className="mb-4">
-                                                    <label className="block text-gray-700 text-sm font-bold mb-2"
-                                                           htmlFor="firstName">
-                                                        First Name
-                                                    </label>
-                                                    <input
-                                                        className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-blue-500"
-                                                        id="firstName"
-                                                        type="text"
-                                                        placeholder="First Name"
-                                                        name="firstName"
-                                                        value={formData.firstName}
-                                                        onChange={(e) => setFormData({
-                                                            ...formData,
-                                                            firstName: e.target.value
-                                                        })}
-                                                    />
-                                                </div>
-                                                <div className="mb-4">
-                                                    <label className="block text-gray-700 text-sm font-bold mb-2"
-                                                           htmlFor="lastName">
-                                                        Last Name
-                                                    </label>
-                                                    <input
-                                                        className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-blue-500"
-                                                        id="lastName"
-                                                        type="text"
-                                                        placeholder="Last Name"
-                                                        name="lastName"
-                                                        value={formData.lastName}
-                                                        onChange={(e) => setFormData({
-                                                            ...formData,
-                                                            lastName: e.target.value
-                                                        })}
-                                                    />
-                                                </div>
-                                                <div className="mb-6">
-                                                    <label className="block text-gray-700 text-sm font-bold mb-2"
-                                                           htmlFor="email">
-                                                        Email
-                                                    </label>
-                                                    <input
-                                                        className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-blue-500"
-                                                        id="email"
-                                                        type="email"
-                                                        placeholder="Email"
-                                                        name="email"
-                                                        value={formData.email}
-                                                        onChange={(e) => setFormData({
-                                                            ...formData,
-                                                            email: e.target.value
-                                                        })}
-                                                    />
-                                                </div>
-                                                <Button variant={"ghost"}>Create</Button>
-                                            </form>
-                                        </div>
-                                    </Container>
+                                        <Container>
+                                            <Row>
+                                                <Col>
+                                            <div className="mx-auto bg-white rounded-lg px-8 pt-6 pb-8 mb-4">
+                                                <form onSubmit={handleCreateEmployee}>
+                                                    <div className="mb-4">
+                                                        <label className="block text-gray-700 text-sm font-bold mb-2"
+                                                               htmlFor="username">
+                                                            Username
+                                                        </label>
+                                                        <input
+                                                            className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-blue-500"
+                                                            id="username"
+                                                            type="text"
+                                                            placeholder="Username"
+                                                            name="username"
+                                                            value={formData.username}
+                                                            onChange={(e) => setFormData({
+                                                                ...formData,
+                                                                username: e.target.value
+                                                            })}
+                                                        />
+                                                    </div>
+                                                    <div className="mb-4">
+                                                        <label className="block text-gray-700 text-sm font-bold mb-2"
+                                                               htmlFor="firstName">
+                                                            First Name
+                                                        </label>
+                                                        <input
+                                                            className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-blue-500"
+                                                            id="firstName"
+                                                            type="text"
+                                                            placeholder="First Name"
+                                                            name="firstName"
+                                                            value={formData.firstName}
+                                                            onChange={(e) => setFormData({
+                                                                ...formData,
+                                                                firstName: e.target.value
+                                                            })}
+                                                        />
+                                                    </div>
+                                                    <div className="mb-4">
+                                                        <label className="block text-gray-700 text-sm font-bold mb-2"
+                                                               htmlFor="lastName">
+                                                            Last Name
+                                                        </label>
+                                                        <input
+                                                            className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-blue-500"
+                                                            id="lastName"
+                                                            type="text"
+                                                            placeholder="Last Name"
+                                                            name="lastName"
+                                                            value={formData.lastName}
+                                                            onChange={(e) => setFormData({
+                                                                ...formData,
+                                                                lastName: e.target.value
+                                                            })}
+                                                        />
+                                                    </div>
+                                                    <div className="mb-6">
+                                                        <label className="block text-gray-700 text-sm font-bold mb-2"
+                                                               htmlFor="email">
+                                                            Email
+                                                        </label>
+                                                        <input
+                                                            className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-blue-500"
+                                                            id="email"
+                                                            type="email"
+                                                            placeholder="Email"
+                                                            name="email"
+                                                            value={formData.email}
+                                                            onChange={(e) => setFormData({
+                                                                ...formData,
+                                                                email: e.target.value
+                                                            })}
+                                                        />
+                                                    </div>
+                                                    <Button variant={"ghost"}>Create</Button>
+                                                </form>
+                                            </div>
+                                                </Col>
+
+                                {/*This is the delete employee thing*/}
+
+                                                <Col>
+                                            <GetDataEmployees></GetDataEmployees>
+                                                </Col>
+
+
+
+                                            </Row>
+                                            </Container>
                         </TabsContent>
+
+
+
+                        {/*</TabsContent>*/}
+
+
+
+
 
                         <TabsContent value="Update Employee">
                             <Container>
+                                <Row>
+                                    <Col>
+
                                 <div className="max-w-md mx-auto bg-white rounded-lg px-8 pt-6 pb-8 mb-4">
                                     <form onSubmit={handleUpdateEmployee}>
                                         <div className="mb-4">
@@ -275,13 +274,51 @@ export const EmployeeManager = () => {
                                         </div>
                                     </form>
                                     </div>
+                                    </Col>
+                                <Col>
+                                <GetDataEmployees></GetDataEmployees>
+                                </Col>
+                                </Row>
+                            </Container>
+                        </TabsContent>
+
+                        <TabsContent value="Delete Employee">
+                            <Container>
+                                <Row>
+                                    <Col>
+                                <div className="mx-auto bg-white rounded-lg px-8 pt-6 pb-8 mb-4">
+                                    <form onSubmit={handleDeleteEmployee}>
+                                        <div className="mb-4">
+                                            <label className="block text-gray-700 text-sm font-bold mb-2"
+                                                   htmlFor="username">
+                                                Delete User by Entering Username
+                                            </label>
+                                            <input
+                                                className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-blue-500"
+                                                id="username"
+                                                type="text"
+                                                placeholder="Username"
+                                                name="username"
+                                                value={formData.username}
+                                                onChange={(e) => setFormData({...formData, username: e.target.value})}
+                                            />
+                                        </div>
+                                        <Button variant={"ghost"}>Delete</Button>
+
+                                    </form>
+                                </div>
+                                    </Col>
+                                    <Col>
+                                        <GetDataEmployees></GetDataEmployees>
+                                    </Col>
+                                </Row>
                             </Container>
                         </TabsContent>
                     </Tabs>
                 </div>
-                <Global_Footer />
+                <Global_Footer/>
             </>
-);
+        );
 };
 
 export default EmployeeManager;
