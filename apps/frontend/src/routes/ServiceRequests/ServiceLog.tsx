bae50ad124ef94893e94ff65a35b18c14d1373b7
maxgossselin <mgosselin2022@gmail.com>
2024-02-14 13:36:11 -0500

    yeah

--- apps/frontend/src/routes/ServiceRequests/ServiceLog.tsx
+++ apps/frontend/src/routes/ServiceRequests/ServiceLog.tsx
@@ -15,18 +15,22 @@ const ServiceLog = () => {
         <div className={"align-content-center"}>
             <br/>
             <br/>
-            <Tabs defaultValue="Flower Service Request" className="w-screen align-content-center">
+            <Tabs defaultValue="External Transport Service Request" className="w-screen align-content-center">
                 <TabsList classname-={"align-content-center"}>
+                    <TabsTrigger value="External Transport Service Request">External Transport Service Request</TabsTrigger>
                     <TabsTrigger value="Flower Service Request">Flower Service Request</TabsTrigger>
                     <TabsTrigger value="Religious Service Request">Religious Service Request</TabsTrigger>
                     <TabsTrigger value="Cleaning Service Request">Cleaning Service Request</TabsTrigger>
                     <TabsTrigger value="Language Service Request">Language Service Request</TabsTrigger>
                     <TabsTrigger value="Internal Transport Service Request">Internal Transport Service Request</TabsTrigger>
-                    <TabsTrigger value="External Transport Service Request">External Transport Service Request</TabsTrigger>
+
                 </TabsList>
 
                 {/*<TabsContent value="Employee List">*/}
 
+                <TabsContent value={"External Transport Service Request"}>
+                    <ExternalTransportServiceLogComponent></ExternalTransportServiceLogComponent>
+                </TabsContent>
                 <TabsContent value="Flower Service Request">
                     <FlowerServiceLogComponent></FlowerServiceLogComponent>
                 </TabsContent>
@@ -42,9 +46,7 @@ const ServiceLog = () => {
                 <TabsContent value={"Language Service Request"}>
                     <LanguageServiceLogComponent></LanguageServiceLogComponent>
                 </TabsContent>
-                <TabsContent value={"External Transport Service Request"}>
-                    <ExternalTransportServiceLogComponent></ExternalTransportServiceLogComponent>
-                </TabsContent>
+
 
                 </Tabs>
             <Global_Footer />
