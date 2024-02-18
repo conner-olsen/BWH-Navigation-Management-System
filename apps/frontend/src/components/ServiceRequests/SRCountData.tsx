c70eadc3c0d065bc83e6ac07cd027b0d542b7b4a
Karish Gupta <karishgupta2003@gmail.com>
2024-02-18 14:46:21 -0500

    Functional pie chart for number of service requests

--- apps/frontend/src/components/ServiceRequests/SRCountData.tsx
+++ apps/frontend/src/components/ServiceRequests/SRCountData.tsx
@@ -1,21 +1,16 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import axios from 'axios';
-//import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
 import { Pie } from 'react-chartjs-2';
+import 'chart.js/auto';
 
-type Status = {
-    labels: string[];
-    datasets: {
-        label: string;
-        data: number[];
-        backgroundColor: string[];
-        borderColor: string[];
-        borderWidth: number;
-    }[];
-};
 
+let countFlower: number = 0;
+let countCleaning: number = 0;
+let countExternal: number = 0;
+let countInternal: number = 0;
+let countLanguage: number = 0;
+let countReligious: number = 0;
 const CreateSRChart = () => {
-    const [chartData, setChartData] = useState<Status>();
 
     useEffect(() => {
         const getCounts = async () => {
@@ -25,72 +20,49 @@ const CreateSRChart = () => {
                         "Content-Type": 'application/json'
                     }
                 });
-                const countFlower = responseFlower.data;
+                console.log(responseFlower.data);
+                countFlower = responseFlower.data;
 
                 const responseReligious = await axios.get("/api/get-stats/religious", {
                     headers: {
                         "Content-Type": 'application/json'
                     }
                 });
-                const countReligious = responseReligious.data;
+                console.log(responseReligious.data);
+                countReligious = responseReligious.data;
+
 
                 const responseCleaning = await axios.get("/api/get-stats/cleaning", {
                     headers: {
                         "Content-Type": 'application/json'
                     }
                 });
-                const countCleaning = responseCleaning.data;
+                console.log(responseCleaning.data);
+                countCleaning = responseCleaning.data;
 
                 const responseInternal = await axios.get("/api/get-stats/internal-transportation", {
                     headers: {
                         "Content-Type": 'application/json'
                     }
                 });
-                const countInternal = responseInternal.data;
+                console.log(responseInternal.data);
+                countInternal = responseInternal.data;
 
                 const responseExternal = await axios.get("/api/get-stats/external-transportation", {
                     headers: {
                         "Content-Type": 'application/json'
                     }
                 });
-                const countExternal = responseExternal.data;
+                console.log(responseExternal.data);
+                countExternal = responseExternal.data;
 
                 const responseLanguage = await axios.get("/api/get-stats/language", {
                     headers: {
                         "Content-Type": 'application/json'
                     }
                 });
-                const countLanguage = responseLanguage.data;
-
-                // Create the chart data
-                const statusData = {
-                    labels: ['Flower', 'Religious', 'Cleaning', 'Internal Transport', 'External Transport', 'Language'],
-                    datasets: [
-                        {
-                            label: '# of Requests per Service',
-                            data: [countFlower, countReligious, countCleaning, countInternal, countExternal, countLanguage],
-                            backgroundColor: [
-                                'rgba(255, 99, 132, 0.2)',
-                                'rgba(54, 162, 235, 0.2)',
-                                'rgba(255, 206, 86, 0.2)',
-                                'rgba(75, 192, 192, 0.2)',
-                                'rgba(153, 102, 255, 0.2)',
-                                'rgba(255, 159, 64, 0.2)',
-                            ],
-                            borderColor: [
-                                'rgba(255, 99, 132, 1)',
-                                'rgba(54, 162, 235, 1)',
-                                'rgba(255, 206, 86, 1)',
-                                'rgba(75, 192, 192, 1)',
-                                'rgba(153, 102, 255, 1)',
-                                'rgba(255, 159, 64, 1)',
-                            ],
-                            borderWidth: 1,
-                        },
-                    ],
-                };
-
-                setChartData(statusData);
+                console.log(responseLanguage.data);
+                countLanguage = responseLanguage.data;
 
             } catch (error) {
                 console.error('Error getting data:', error);
@@ -100,11 +72,37 @@ const CreateSRChart = () => {
         getCounts().then();
     }, []); // Empty dependency array to ensure the effect runs only once
 
+    // Create the chart data
+    const statusData = {
+        labels: ['Flower', 'Religious', 'Cleaning', 'Internal Transport', 'External Transport', 'Language'],
+        datasets: [
+            {
+                label: '# of Requests per Service',
+                data: [countFlower, countReligious, countCleaning, countInternal, countExternal, countLanguage],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)',
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)',
+                ],
+                borderWidth: 1,
+            },
+        ],
+    };
+
     return (
         <div>
-            {chartData && (
-                <Pie className="w-[1000px]" data={chartData} />
-            )}
+            <Pie data={statusData} />
         </div>
     );
 };
