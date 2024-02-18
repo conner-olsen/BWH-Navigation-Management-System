3e0a4b867aa410cc2cfb4ae83fff05483bfb1ae5
ceciherriman <herrimanceci@gmail.com>
2024-02-18 13:31:44 -0500

    dfs testing -- results depend on when edges are added

--- apps/backend/tests/dfs.test.ts
+++ apps/backend/tests/dfs.test.ts
@@ -3,7 +3,7 @@ import { Graph } from 'common/src/graph-structure.ts';
 import { Node } from 'common/src/graph-structure.ts';
 import {dfsPathfinding} from "common/src/PathfindingMethod.ts";
 
-/////////////////manual bfs testing:
+/////////////////manual dfs testing:
 const graph = new Graph();
 
 // Add nodes and edges to the graph (similar to the example in graph-structure.ts)
@@ -12,17 +12,23 @@ const node1 = new Node("1", 1, 1, "1", "1", "1", "1", "1");
 const node2 = new Node("2", 2, 2, "2", "2", "2", "2", "2");
 const node3 = new Node("3", 3, 3, "3", "3", "3", "3", "3");
 const node4 = new Node("4", 4, 3, "3", "3", "4", "4", "4");
+const node5 = new Node("5", 4, 2, "3", "3", "5", "5", "5");
+
+
 // add nodes to graph
 graph.addNode(node1);
 graph.addNode(node2);
 graph.addNode(node3);
 graph.addNode(node4);
-
+graph.addNode(node5);
 
 // add edges to graph
 graph.addEdge("1", "2");
+graph.addEdge("3", "4");
 graph.addEdge("2", "3");
 graph.addEdge("3", "1");
+graph.addEdge("1", "4");
+
 
 const startNode = '1';
 const endNode = '3';
@@ -38,6 +44,10 @@ test("find path 1 2 3", () => {
   expect(graph.runPathfinding(startNode, endNode)).toStrictEqual(answer);
 });
 
+test("find path 3 4", () => {
+  expect(graph.runPathfinding("3", "4")).toStrictEqual(["3", "1", "4"]);
+});
+
 //test the same node
 test("find path 1 1", () => {
   expect(graph.runPathfinding("1", "1")).toStrictEqual(["1"]);
@@ -45,7 +55,7 @@ test("find path 1 1", () => {
 
 //test backwards
 test("find path 3 2", () => {
-  expect(graph.runPathfinding("3", "2")).toStrictEqual(["3", "2"]);
+  expect(graph.runPathfinding("3", "2")).toStrictEqual(["3", "1", "2"]);
 });
 
 //test empty
