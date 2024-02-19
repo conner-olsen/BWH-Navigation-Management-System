1f60c95d71c7bd1f3e79f90096091ab31e117cfd
ceciherriman <herrimanceci@gmail.com>
2024-02-19 18:26:34 -0500

    removed name display of halls

--- apps/frontend/src/components/maps/MapDisplay.tsx
+++ apps/frontend/src/components/maps/MapDisplay.tsx
@@ -161,7 +161,7 @@ function MapDisplay({
     };
 
     const displayName = (node: Node) => {
-        if (doDisplayNames && (node.floor == floor)) {
+        if ((doDisplayNames && (node.floor == floor)) && !(node.nodeType == "HALL")) {
             return (
                 <text className="font-bold dark:invert" x={node.xCoord - 65} y={node.yCoord - 20} fill="black">
                     {node.shortName}
