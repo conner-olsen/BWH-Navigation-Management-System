e71a7bbaacb3fe3a27f695466fe9d924f557089a
ARealConner <81334211+ARealConner@users.noreply.github.com>
2024-02-19 14:46:57 -0500

    Refactored CSV file paths in backend.
    The changes include updating the relative paths to the CSV files for Edge and Node data in the backend csv-handler. Also, the CSV files were moved from the 'packages' directory to the 'apps/backend/data' directory for a more simplified project structure.

--- apps/backend/src/routes/csv-handler.ts
+++ apps/backend/src/routes/csv-handler.ts
@@ -14,7 +14,7 @@ const router: Router = express.Router();
 router.post("/", async (req: Request, res: Response) => {
   try {
     // language=file-reference - Read the CSV string from the request body
-    const relativeNodePath = "../../../../packages/common/dev/data/csv/L1Nodes.csv";
+    const relativeNodePath = "../../data/csv/L1Nodes.csv";
     const absoluteNodePath = path.join(__dirname, relativeNodePath);
     const csvFileNode = fs.readFileSync(absoluteNodePath, "utf-8");
 
@@ -50,7 +50,7 @@ router.post("/", async (req: Request, res: Response) => {
     });
 
     // language=file-reference Read the CSV string from the request body
-    const absolutePath = path.join(__dirname, "../../../../packages/common/dev/data/csv/L1Edges.csv");
+    const absolutePath = path.join(__dirname, "../../data/csv/L1Edges.csv");
     const csvFile = fs.readFileSync(absolutePath, "utf-8");
 
     // Parse the CSV string to an array of CSVRow objects
