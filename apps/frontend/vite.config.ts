784da0dace5c896118159380c172f887aac31a94
ARealConner <81334211+ARealConner@users.noreply.github.com>
2024-02-19 22:12:27 -0500

    Update vite.config.ts with new settings and plugins.
    The vite.config.ts file has been updated to include new settings for server, build configuration, and new plugins (react and eslint). This adjustment provides improved development environment configuration, appropriate routing for API requests, and includes eslint for code quality checks.

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,5 +1,24 @@
-import { defineProject } from 'vitest/config'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import eslint from "vite-plugin-eslint";
 
-export default defineProject({
-  test: {},
-})
+// https://vitejs.dev/config/
+export default defineConfig({
+  resolve: {
+    preserveSymlinks: true,
+  },
+  server: {
+    host: "0.0.0.0",
+    port: parseInt(<string>process.env.PORT),
+    proxy: {
+      "/api": process.env.BACKEND_SOURCE + ":" + process.env.BACKEND_PORT,
+    },
+    watch: {
+      usePolling: true,
+    },
+  },
+  build: {
+    outDir: "build",
+  },
+  plugins: [react(), eslint()],
+});
