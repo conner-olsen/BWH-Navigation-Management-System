03fc1010c90deac6c2f3b7bbf300474aa79594c5
jdwalden74 <jdwalden2@gmail.com>
2024-02-19 19:49:39 -0500

    added way to get specfic service request info

--- apps/frontend/src/components/NodeServiceRequestComponent.tsx
+++ apps/frontend/src/components/NodeServiceRequestComponent.tsx
@@ -7,12 +7,47 @@ import {
 import {Table, TableBody, TableCell, TableHead, TableHeader, TableRow} from "./ui/table.tsx";
 import {Container} from "react-bootstrap";
 
-function seperateSpecific(item: ServiceRequest): string{
 
-    const { nodeId,employeeUser, status, priority,id, ...specificData } = item;
+function processServiceRequest(serviceRequest: ServiceRequest) {
+    // Check if it includes a flower service request
+    if (serviceRequest.flowerServiceRequests) {
+        // Process the flower service request
+        return("flowerServiceRequest");
+    }
 
-    console.log(specificData);
-    return(JSON.stringify(specificData));
+    // Check if it includes a cleaning service request
+    if (serviceRequest.cleaningServiceRequest) {
+        // Process the cleaning service request
+        return("cleaningServiceRequest");
+    }
+
+    // Check if it includes an external transportation service request
+    if (serviceRequest.externalTransportationServiceRequest) {
+        // Process the external transportation service request
+        return("externalTransportationServiceRequest");
+    }
+
+    // Check if it includes an internal transport service request
+    if (serviceRequest.internalTransportServiceRequest) {
+        // Process the internal transport service request
+        return("internalTransportServiceRequest");
+    }
+
+    // Check if it includes a language interpreter service request
+    if (serviceRequest.languageInterpreterServiceRequest) {
+        // Process the language interpreter service request
+        return("languageInterpreterServiceRequest");
+    }
+
+    // Check if it includes a religious service request
+    if (serviceRequest.religiousServiceRequest) {
+        // Process the religious service request
+        return("religiousServiceRequest");
+    }
+    else{
+
+        return("");
+    }
 }
 
 function GenerateTableRowsServices(tableData: ServiceRequest[]): JSX.Element[] {
@@ -22,7 +57,7 @@ function GenerateTableRowsServices(tableData: ServiceRequest[]): JSX.Element[] {
                 <TableCell>{item.priority}</TableCell>
                 <TableCell>{item.status}</TableCell>
                 <TableCell>{item.employeeUser}</TableCell>
-                <TableCell>{seperateSpecific(item)}</TableCell>
+                <TableCell>{processServiceRequest(item)}</TableCell>
             </TableRow>
 
         ));
