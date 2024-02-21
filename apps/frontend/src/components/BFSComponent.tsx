6c18d037bad7a2a8717990ead32d47e497a8c523
ceciherriman <herrimanceci@gmail.com>
2024-02-21 11:53:56 -0500

    debugging

--- apps/frontend/src/components/BFSComponent.tsx
+++ apps/frontend/src/components/BFSComponent.tsx
@@ -218,7 +218,7 @@ export function BFSComponent() {
                     returnBooleans[i - 1] = true;
                 } else {
                     returnBooleans[i] = true;
-                    previousIsElevOrStair= false;
+                    previousIsElevOrStair = true;
                 }
             } else {
                 returnBooleans[i] = false;
