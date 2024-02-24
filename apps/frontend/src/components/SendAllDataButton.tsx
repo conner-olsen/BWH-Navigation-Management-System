8984d52fadd11b7523d64d5c2bbe64cc74558b38
Karish Gupta <karishgupta2003@gmail.com>
2024-02-24 15:07:39 -0500

    front end functional

--- apps/frontend/src/components/SendAllDataButton.tsx
+++ apps/frontend/src/components/SendAllDataButton.tsx
@@ -1,53 +1,55 @@
-import {ChangeEvent, useState} from "react";
+import {ChangeEvent, useCallback, useEffect, useState} from "react";
 
 const SendAllDataButton = () => {
 
     const [selectedFiles, setSelectedFiles] = useState<File[]>([]);
 
-    const handleSendAllData = async () => {
-        const filemap = {
-            file1: JSON.stringify(selectedFiles[0]),
-            file2: JSON.stringify(selectedFiles[1]),
-            file3: JSON.stringify(selectedFiles[2])
-        };
+    const handleSendAllData = useCallback(async () => {
+        let filemap: {[key: string]: string} = {};
 
+        await Promise.all(selectedFiles.map(async (file, index) => {
+            const fileContent = await file.text();
+            filemap[`file${index}`] = fileContent;
+        }));
 
         try {
+            console.log(filemap);
+
             const res1 = await fetch("/api/single-button-import", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json", // Set the appropriate content type
                 },
-                body: JSON.stringify(filemap) // Send an empty JSON object
+                body: JSON.stringify(filemap)
             });
 
             console.log(res1);
         } catch (error) {
             console.error("Error:", error);
         }
+    }, [selectedFiles]);
 
+    useEffect(() => {
+        if (selectedFiles.length > 0) {
+            //console.log(selectedFiles);
+            handleSendAllData();
+        }
+    }, [selectedFiles, handleSendAllData]);
+
+    const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
+        if (event.target.files) {
+            const files: FileList = event.target.files;
+            setSelectedFiles([...selectedFiles, ...Array.from(files)]);
+        }
     };
 
-
-
-
-        // Function to handle file selection
-        const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
-            if (event.target.files) {
-                const files: FileList = event.target.files;
-                setSelectedFiles([...selectedFiles, ...Array.from(files)]);
-                handleSendAllData();
-            }
-        };
 
 
     return (
         <div>
             <input type="file" multiple onChange={handleFileChange}/>
             <div>
-                {selectedFiles.length > 0 && (
-                    <p>Import All Files:</p>
-                )}
+                <p>Import All Files:</p>
             </div>
         </div>
     );
