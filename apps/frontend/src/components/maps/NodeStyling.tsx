eb717bd24680634937775c302a000fd8e8b7292d
StevenGao <gaoxc602@gmail.com>
2024-02-27 17:24:08 -0500

    working notification for service request

--- apps/frontend/src/components/maps/NodeStyling.tsx
+++ apps/frontend/src/components/maps/NodeStyling.tsx
@@ -81,7 +81,7 @@ export function NodeStyling(props: {
 
     const boxShadowColor = getNodeCount(props.node);
 
-    return <g>
+    return( <g>
 
         <rect className={HeatMap ? "" : "fill-blue-100 dark:fill-blue-900"}
               x={props.node.xCoord - props.iconSize.width / 2}
@@ -112,6 +112,6 @@ export function NodeStyling(props: {
             />
         )}
         {props.element}
-    </g>;
+    </g>);
 }
 
