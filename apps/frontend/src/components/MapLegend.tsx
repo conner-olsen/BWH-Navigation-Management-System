74b1289bd7346268c2b472e4b0d4484da42e1f0a
Karish Gupta <karishgupta2003@gmail.com>
2024-02-27 21:14:40 -0500

    Map legend done

---
+++ apps/frontend/src/components/MapLegend.tsx
@@ -0,0 +1,78 @@
+import {HoverCard, HoverCardContent, HoverCardTrigger} from "./ui/hovercard.tsx";
+import {iconPaths} from "./maps/IconPath.tsx";
+
+const MapLegend = () => {
+    return (
+        <div
+            className={`fixed bottom-[0px] mb-2.5 mr-2.5 right-[0px] flex flex-col bg-background rounded-xl z-10`}>
+            <HoverCard openDelay={100}>
+                <HoverCardTrigger className="w-[80px] h-[80px] flex justify-center items-center no-underline text-foreground relative cursor-pointer group">
+                    <p className="absolute bottom-[5px] text-[12px] font-bold m-0">Legend</p>
+                </HoverCardTrigger>
+
+                <HoverCardContent side="right" className="pb-2 z-40">
+                    <p className="text-xs text-center">Legend</p>
+
+                    <div className="flex items-center">
+                        <img id={"CONF"} src={iconPaths.CONF} alt="Conference Room" className="w-6 h-6 mr-2 dark:invert"/>
+                        <label id={"CONF"}>Conference Room</label>
+                    </div>
+
+                    <div className="flex items-center">
+                        <img id={"DEPT"} src={iconPaths.DEPT} alt="Department Room" className="w-6 h-6 mr-2 dark:invert"/>
+                        <label id={"DEPT"}>Department Room</label>
+                    </div>
+
+                    <div className="flex items-center">
+                        <img id={"ELEV"} src={iconPaths.ELEV} alt="Elevator" className="w-6 h-6 mr-2 dark:invert"/>
+                        <label id={"ELEV"}>Elevator</label>
+                    </div>
+
+                    <div className="flex items-center">
+                        <img id={"EXIT"} src={iconPaths.EXIT} alt="Exit" className="w-6 h-6 mr-2 dark:invert"/>
+                        <label id={"EXIT"}>Exit</label>
+                    </div>
+
+                    <div className="flex items-center">
+                        <img id={"INFO"} src={iconPaths.INFO} alt="Info Desk" className="w-6 h-6 mr-2 dark:invert"/>
+                        <label id={"INFO"}>Info Desk</label>
+                    </div>
+
+                    <div className="flex items-center">
+                        <img id={"LABS"} src={iconPaths.LABS} alt="Laboratory" className="w-6 h-6 mr-2 dark:invert"/>
+                        <label id={"LABS"}>Laboratory</label>
+                    </div>
+
+                    <div className="flex items-center">
+                        <img id={"REST"} src={iconPaths.REST} alt="Restroom" className="w-6 h-6 mr-2 dark:invert"/>
+                        <label id={"REST"}>Restroom</label>
+                    </div>
+
+                    <div className="flex items-center">
+                        <img id={"BATH"} src={iconPaths.BATH} alt="Bathroom" className="w-6 h-6 mr-2 dark:invert"/>
+                        <label id={"BATH"}>Bathroom</label>
+                    </div>
+
+                    <div className="flex items-center">
+                        <img id={"RETL"} src={iconPaths.RETL} alt="Retail" className="w-6 h-6 mr-2 dark:invert"/>
+                        <label id={"RETL"}>Retail</label>
+                    </div>
+
+                    <div className="flex items-center">
+                        <img id={"STAI"} src={iconPaths.STAI} alt="Stairs" className="w-6 h-6 mr-2 dark:invert"/>
+                        <label id={"STAI"}>Stairs</label>
+                    </div>
+
+                    <div className="flex items-center">
+                        <img id={"SERV"} src={iconPaths.SERV} alt="Service" className="w-6 h-6 mr-2 dark:invert"/>
+                        <label id={"SERV"}>Service</label>
+                    </div>
+
+                </HoverCardContent>
+            </HoverCard>
+
+        </div>
+    );
+};
+
+export default MapLegend;
